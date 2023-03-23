6a7f74eb96b866204194d865dfb0fe3134e1c418
= <=>
2023-03-23 17:04:57 -0300

    up

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -1,10 +1,10 @@
-# Use readarray para ler a string em um array
-readarray -t array <<< "$files"
-
 if [ -a "$files" ]; then
     echo "é um array suave"
 else
     echo "não é um array, deu ruim"
 fi
+
+# Use readarray para ler a string em um array
+readarray -t array <<< "$files"
 # Imprima o array
 printf '%s\n' "${array[@]}"
