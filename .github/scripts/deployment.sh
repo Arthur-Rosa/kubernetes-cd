216f1e5078ac92b24d0c43838af2ad686c544688
= <=>
2023-03-23 17:03:28 -0300

    run pipe

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -1,5 +1,10 @@
 # Use readarray para ler a string em um array
 readarray -t array <<< "$files"
 
+if [ -a "$files" ]; then
+    echo "é um array suave"
+else
+    echo "não é um array, deu ruim"
+fi
 # Imprima o array
 printf '%s\n' "${array[@]}"
