1eb22085e6ee8b973ea19ce160f26bab4ac94ec4
= <=>
2023-03-23 18:02:10 -0300

    updta

---
+++ .github/scripts/connect.sh
@@ -0,0 +1,4 @@
+aws configure set aws_access_key_id ${aws_acess}
+aws configure set aws_secret_access_key ${aws_secret}
+aws configure set default.region ${aws_region}
+aws configure set default.output ${aws_output}
--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -7,12 +7,5 @@ for f in "${array[@]}"
 do
     path="./$f"
     
-    aws configure
-
-
-    echo $aws_acess
-    echo $aws_secret
-    echo "us-east-1"
-    echo "json"
 
 done
