67d5a170b3f1ba689a75649520ce63e056c8d72a
= <=>
2023-03-23 17:50:44 -0300

    test

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -3,11 +3,14 @@ IFS=' '
 array=($files)
 IFS=$OLDIFS
 
-echo $teste
-
 for f in "${array[@]}" 
 do
     path="./$f"
     
-
+    aws configure
+    $aws_acess
+    $aws_secret
+    echo "us-east-1"
+    echo "json"
+    
 done
