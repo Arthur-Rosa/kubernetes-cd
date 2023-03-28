196da1480e8d116a4a7e9842ed9c532eaa1206f2
= <=>
2023-03-28 20:16:44 -0300

    008

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -12,14 +12,15 @@ do
     aws eks --region eu-north-1 update-kubeconfig --name basic-cluster
 
     aws eks update-kubeconfig --name basic-cluster --region aws-region
+    
+    eksctl utils write-kubeconfig --kubeconfig=./.github/config/kubeconfig.yaml --cluster=basic-cluster
 
-    eksctl utils write-kubeconfig --kubeconfig=./.github/config/kubeconfig.yaml --profile=test
     echo "##############################################################"
     echo "Pré criando File"
 
     echo "##############################################################"
     echo "Iniciando criação" 
 
-    eksctl create nodegroup --config-file=$path
+    eksctl create nodegroup --config-file=$path --profile 
 
 done
