92c58c21be81236e33d74ccc97aacc009d513033
= <=>
2023-03-28 20:07:09 -0300

    006

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -11,17 +11,19 @@ do
     
     aws eks --region eu-north-1 update-kubeconfig --name basic-cluster
 
-    aws eks update-kubeconfig --name basic-cluster --region aws-region --role-arn arn:aws:sts::804006830115:assumed-role/az_lighthouse_support_n2/arthur.rosa@aquarelacs.com.br
+    aws eks update-kubeconfig --name basic-cluster --region aws-region
+
+    eksctl utils write-kubeconfig --kubeconfig=./.github/config/kubeconfig.yaml --profile=test
     echo "##############################################################"
     echo "Pegando File"
 
-    cat $path
+    
 
     echo " "
 
     echo "##############################################################"
     echo "Iniciando criação" 
 
-    eksctl create nodegroup --config-file=$path
+    eksctl create nodegroup --config-file=$path --profile 
 
 done
