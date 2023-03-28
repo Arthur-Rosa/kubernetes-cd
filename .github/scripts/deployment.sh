9239a47d6a0404aa9037ef58a7778c54d0ebae9e
= <=>
2023-03-28 19:49:31 -0300

    004

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -9,6 +9,7 @@ do
     
     aws eks --region eu-north-1 update-kubeconfig --name basic-cluster
 
+    aws eks update-kubeconfig --name basic-cluster --region aws-region --role-arn arn:aws:sts::804006830115:assumed-role/az_lighthouse_support_n2/arthur.rosa@aquarelacs.com.br
     echo "##############################################################"
     echo "Pegando File"
 
